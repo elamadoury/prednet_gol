b8ae8be3d32454fc81bc895d6a727c0d352a83e7
DESKTOP-7L9MC4G\lana <lana.sinapayen@gmail.com>
2019-03-08 18:40:57 +0900

    add prednet to repo

---
+++ prednet/disney_settings.py
@@ -0,0 +1,9 @@
+# Where data will be saved if you run process_kitti.py
+DATA_DIR = 'D:/ShareData/disney_data/'#'./kitti_data/'
+
+# Where model weights and config will be saved if you run kitti_train.py
+# If you directly download the trained weights, change to appropriate path.
+WEIGHTS_DIR = './model_data_keras2/'
+
+# Where results (prediction plots and evaluation file) will be saved.
+RESULTS_SAVE_DIR = './naive_disney_results/'
