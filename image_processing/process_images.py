b69f36bd85b193d8a49715389c85ccddab8b539e
Lana <lana.sinapayen@gmail.com>
2019-04-03 06:21:51 +0900

    better file structure

--- image_processing/process_images.py
+++ image_processing/process_images.py
@@ -10,11 +10,21 @@ from imageio import imread
 from scipy.misc import imresize
 import hickle as hkl
 
+
+usage = 'Usage: python {} SETTINGS_FILE [--help]'.format(__file__)
+parser = argparse.ArgumentParser(description='This script is to generate .hkl files for train and test.',
+                                 usage=usage)
+parser.add_argument('settings_file', action='store', nargs=None, 
+                    type=str, help='dataset settings file with data path.')
+args = parser.parse_args()
+
 desired_im_sz = (128, 160)
+
+from args.settings_file import *
 
 # Processes images and saves them in train, val, test splits.
 def process_data():
-    DATA_DIR = "D:/ShareData/disney_data/"
+    #DATA_DIR = args.settings_file
     im_dir = DATA_DIR + "images/"
     split = "test"
     im_list = []
@@ -42,5 +52,26 @@ def process_im(im, desired_sz):
     return im
 
 
+#store images in a reverse order
+def reverse_process_data():
+    im_dir = DATA_DIR + "images/"
+    split = "test"
+    im_list = []
+    source_list = []  # corresponds to recording that image came from
+    for i in reversed(range(36378,(36378+50))): 
+        image_name = "frame_" + str(i).zfill(5)  + ".jpg" 
+        im_list += [im_dir + image_name]
+        source_list += [im_dir]
+
+    print( 'Creating ' + split + ' data: ' + str(len(im_list)) + ' images')
+    X = np.zeros((len(im_list),) + desired_im_sz + (3,), np.uint8)
+    for i, im_file in enumerate(im_list):
+        im = imread(im_file)
+        X[i] = process_im(im, desired_im_sz)
+
+    hkl.dump(X, os.path.join(DATA_DIR, 'X_' + split + '.hkl'))
+    hkl.dump(source_list, os.path.join(DATA_DIR, 'sources_' + split + '.hkl'))
+
 if __name__ == '__main__':
-    process_data()
+    #process_data()
+    reverse_process_data()
--- prednet/datasets_settings/fpsi_settings.py
+++ prednet/datasets_settings/fpsi_settings.py
@@ -1,9 +1,9 @@
 # Where data will be saved if you run process_kitti.py
-DATA_DIR = 'D:/ShareData/fpsi_data/'#'./kitti_data/'
+DATA_DIR = '../datasets/images/fpsi_data/'#'./kitti_data/'
 
 # Where model weights and config will be saved if you run kitti_train.py
 # If you directly download the trained weights, change to appropriate path.
-WEIGHTS_DIR = './model_data_keras2/'
+WEIGHTS_DIR = './fpsi_weights/'
 
 # Where results (prediction plots and evaluation file) will be saved.
 RESULTS_SAVE_DIR = './fpsi_results/'
